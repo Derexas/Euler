4f4752d194013a5d6ce79d61b7156bcabf45b239
derexas <derexas@gmail.com>
2017-07-02 13:29:14 +0900

    finally solved the 92

--- euler92.py
+++ euler92.py
@@ -1,51 +1,88 @@
-import time, math
+"""
 
-t = time.time()
+"""
 
-n = 3
-sqs = [str(x) for x in range(n)]
-
-def nbcombinaisn(n, k): # (n+k-1)!/[k!*(n-1)!]
-	denum = (math.factorial(k)*math.factorial(n-1))
-	return math.factorial(n+k-1)/denum
-
-def combinaison(k):
-	a = 0
-	bll = 1
-	tab = [sqs]
-	for l in range(k):
-		tab.append([])
-		t = []
-		bl = 0
-		ll = len(tab[l])
-		for i in range(n):
-
-			t += [sqs[i]+str(x) for x in tab[l][bl:ll]]
-			#bl += 1#(n-1)**l
-			print(bl)
-		bll = bl
-		tab[l+1] += t
-		print(tab[l])
-	print(len(tab[l]))
+from itertools import combinations_with_replacement
+import math
+import time
 
 
-result = 0
+start = time.time()
 
-result = nbcombinaisn(n, 4)
-combinaison(4)
+count = 0
+digits = [i for i in range(0,10)]
+sqrs = [i**2 for i in digits]
 
-print(result, (time.time() - t)*1000)
 
-#100-999 = 899
-#100,101,102,..,111,..,199,..,578,..,999 = C(10,7)
-#C(n,k) = (n+k-1)!/(k!(n-1)!)
-#001,011,111,000
-#00,01,02,03,11,12,13,22,23,33 = 4+3+2+1 = (4+2-1!)/2!(4-1)! = 10
-#000,001,002,003,011,012,013,022,023,033,
-#111,112,113,122,123,133,222,223,233,333
-#(4+3+2+1)+(3+2+1)+(2+1)+(1) = (4+3-1)!/3!(4-1)! = 720/36 = 20
+def nCr(n,r):
+    f = math.factorial
+    return f(n) / f(r) / f(n-r)
 
-# 3: 0,1,2 ; 2(2*1-0),3(2*2-1) 
-# 5: 0,1,2,3,4 ; 4(4*1-0),7(4*2-1),10(4*3-2)
-# 5: 2,3,4 ; (2,3,4),6,(2,3,4),12,(3,4)
-# 3: 
+def sum_sqdigits(n):
+	s = 0
+	while n:
+		s, n = s + sqrs[n % 10], n // 10
+	return s
+
+# pre-process all the possible nCr which are going to be used
+ncrs = [[0]*10 for _ in range(10)]
+for n in range(10):
+	for r in range(n):
+		ncrs[n][r] = nCr(n, r)
+
+n, r = len(digits), 7
+# number of combinations with repetitions
+ncwr = math.factorial(n+r-1)/(math.factorial(r)*math.factorial(n-1))
+# number of combinations
+nc = math.factorial(r)
+
+n, k = digits, r
+# get a list of all combinations with replacement
+cwr = list(combinations_with_replacement(n,k))
+
+sqs = [0]*len(cwr) #contains the sum of square digits of each set from cwr
+rs = [0]*len(cwr) #number of numbers made out of each set from cwr ((1,2) > 12,21:2)
+repets = [[0]*10 for i in cwr] #number of time a digits is present in each set
+i = 0
+for c in cwr:
+	for b in c:
+		repets[i][b] += 1
+		sqs[i] += sqrs[b]
+	i += 1
+
+maxssq = (r*9**2 + 1) #max sum of sq digits of numbers bellow 10M
+yn = [-1]*maxssq #array of whether a number 'goes' to 89 or not (-1 if unknow)
+yn[89] = True
+yn[0] = yn[1] = False #set them at False because they 'loop'
+
+i = 0
+for sq in sqs: #for each sum of square digits
+	originalsq = sq
+	# process rs[i]
+	ccc = r
+	cc = 1
+	for rr in repets[i]:
+		cc *= nCr(ccc, rr)
+		ccc -= rr
+	rs[i] = cc
+	# if True, update count
+	if yn[sq] == True:
+		count += rs[i]
+	# if maybe True..
+	if yn[sq] == -1:
+		m = []
+		#..process the digits sums until being sure
+		while yn[sq] == -1:
+			sq = sum_sqdigits(sq)
+			m.append(sq)
+		if yn[sq] == True:
+			count += rs[i] #update count
+			for osq in m: #set all the previous parsed numbers to True
+				yn[osq] = True
+			#print(originalsq, rs[i], cwr[i], m, repets[i], i)
+	i += 1
+
+print(count)
+
+end = time.time()
+print((end - start)*1000)
